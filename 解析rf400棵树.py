c39b0fa5c1145c6a1acf6e0e9d149680bddd7c73
sudazd <115388462+sudazd@users.noreply.github.com>
2026-07-14 10:41:15 +0800

    Add files via upload
    为了实现STRUT迁移学习算法，需要首先将随机森林模型中的每棵决策树解析为可编辑的数据结构。由于STRUT算法需要修改节点阈值，因此不能直接调用Scikit-learn封装好的RandomForest模型，而需要将训练后的森林导出为CSV文件，并重新解析生成树结构。因此，需要验证CSV解析后的树结构与原始RandomForest中的决策树保持完全一致，以保证后续迁移过程中模型结构不发生改变。

---
+++ 解析rf400棵树.py
@@ -0,0 +1,166 @@
+# -*- coding:utf-8 -*-
+
+"""
+解析RandomForest结构
+适用于sklearn RandomForestClassifier
+
+作者：STRUT-RF
+"""
+
+import os
+import joblib
+import numpy as np
+import pandas as pd
+
+#===========================
+# 路径
+#===========================
+
+MODEL_PATH = r"E:\Data\Model\RF_Plateau\rf_model.pkl"
+
+OUTPUT = r"E:\Data\Model\RF_Plateau\STRUT_rf\TreeParser"
+
+os.makedirs(OUTPUT,exist_ok=True)
+
+#===========================
+# 读取模型
+#===========================
+
+print("="*60)
+print("读取随机森林模型...")
+print("="*60)
+
+rf = joblib.load(MODEL_PATH)
+
+print(rf)
+
+print()
+
+print("树数量：",len(rf.estimators_))
+
+print("类别数：",rf.n_classes_)
+
+print("输入变量数：",rf.n_features_in_)
+
+print()
+
+#===========================
+# 因子名称
+#===========================
+
+feature_names = [
+
+    "elev",
+    "slope",
+    "aspect",
+    "plan_curv",
+    "profile_curv",
+    "curvature",
+    "T",
+    "rain",
+    "FVC"
+
+]
+from sklearn.tree import export_text
+
+print("=" * 60)
+print("第一棵树结构（用于验证）")
+print("=" * 60)
+
+tree_text = export_text(
+    rf.estimators_[0],
+    feature_names=feature_names
+)
+
+print(tree_text)
+#===========================
+# 开始解析
+#===========================
+
+all_nodes=[]
+
+print("="*60)
+print("开始解析400棵树...")
+print("="*60)
+
+for tree_id,estimator in enumerate(rf.estimators_):
+
+    tree=estimator.tree_
+
+    n_nodes=tree.node_count
+
+    print(f"Tree {tree_id+1:03d} : {n_nodes} Nodes")
+
+    for node in range(n_nodes):
+
+        feature=tree.feature[node]
+
+        if feature==-2:
+
+            feature_name="Leaf"
+
+        else:
+
+            feature_name=feature_names[feature]
+
+        all_nodes.append({
+
+            "Tree":tree_id,
+
+            "Node":node,
+
+            "FeatureIndex":feature,
+
+            "FeatureName":feature_name,
+
+            "Threshold":tree.threshold[node],
+
+            "LeftChild":tree.children_left[node],
+
+            "RightChild":tree.children_right[node],
+
+            "Samples":tree.n_node_samples[node],
+
+            "Impurity":tree.impurity[node],
+
+            "Value0":tree.value[node][0][0],
+
+            "Value1":tree.value[node][0][1]
+
+        })
+
+#===========================
+# 保存CSV
+#===========================
+
+df=pd.DataFrame(all_nodes)
+
+csv_path=os.path.join(
+
+OUTPUT,
+
+"RF_Tree_Structure.csv"
+
+)
+
+df.to_csv(
+
+csv_path,
+
+index=False,
+
+encoding="utf-8-sig"
+
+)
+
+print()
+
+print("="*60)
+
+print("解析完成")
+
+print("CSV：",csv_path)
+
+print("总节点数：",len(df))
+
+print("="*60)
